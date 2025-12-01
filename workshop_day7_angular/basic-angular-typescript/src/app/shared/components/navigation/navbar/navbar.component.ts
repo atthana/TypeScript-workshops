f0dc77e5cff90dbf67e65c2b40d1d37da415b4a8
atthana phiphatthanaloetmongkhon <atthana.p@codium.co>
2025-12-01 23:12:03 +0700

    FE: Update navbar

--- workshop_day7_angular/basic-angular-typescript/src/app/shared/components/navigation/navbar/navbar.component.ts
+++ workshop_day7_angular/basic-angular-typescript/src/app/shared/components/navigation/navbar/navbar.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, NgClass, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  // ตัวแปร properties
+  isMobileMenuOpen = false;
+  showServicesMenu = false;
+  showMobileServicesMenu = false;
+  private closeTimeout: any = null;
+  
+  // ฟังก์ชัน toggleMobileMenu
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+  
+  // ฟังก์ชัน toggleServicesMenu สำหรับเมนูบริการ
+  toggleServicesMenu() {
+    this.showMobileServicesMenu = !this.showMobileServicesMenu;
+  }
+  
+  // ฟังก์ชัน onmouseEnter สำหรับเมนูบริการ (Mobile)
+  onMouseEnter() {
+    if (this.closeTimeout) {
+      clearTimeout(this.closeTimeout);
+      this.closeTimeout = null;
+    }
+    this.showServicesMenu = true;
+  }
+  
+  // ฟังก์ชัน onMouseLeave สำหรับเมนูบริการ (Desktop)
+  onMouseLeave() {
+    this.closeTimeout = setTimeout(() => {
+      this.showServicesMenu = false;
+    }, 200);
+  }
 }
